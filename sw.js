9642e92e5a68b430ec379b7b11167e79fa160999
resatozkaya <66208813+resatozkaya@users.noreply.github.com>
2026-03-25 22:59:59 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function() {
+  // Şimdilik boş bırakıyoruz
+});
